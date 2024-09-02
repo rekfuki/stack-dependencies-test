2298243cae50b9ec7f222df40b94030672c0a459
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-02 13:31:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "spacelift_stack" "Stack-A" {
+resource "spacelift_stack" "stack-a" {
   name        = "Dependencies stack A"
   repository   = "stack-dependencies-test"
   project_root = "stack-a"
   branch       = "main"
 }
 
-resource "spacelift_stack" "Stack-A" {
+resource "spacelift_stack" "stack-b" {
   name        = "Dependencies stack B"
   description = "Your first stack managed by Terraform"
   repository   = "stack-dependencies-test"

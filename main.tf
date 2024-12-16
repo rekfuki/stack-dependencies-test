bd3e7a6e51c964a10097116b9e649957fc290d61
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-16 15:13:42 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,14 @@ resource "spacelift_stack" "stack-b" {
   project_root = "stack-b"
   branch       = "main"
 }
+
+resource "spacelift_stack_dependency" "dependency" {
+  stack_id            = spacelift_stack.stack-b.id
+  depends_on_stack_id = spacelift_stack.stack-a.id
+}
+
+resource "spacelift_stack_dependency_reference" "reference" {
+  stack_dependency_id = spacelift_stack_dependency.dependency.id
+  output_name         = "DB_CONNECTION_STRING"
+  input_name          = "TF_VAR_APP_DB_URL"
+}

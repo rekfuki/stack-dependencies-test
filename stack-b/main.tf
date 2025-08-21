83d5f59c7e9f734246a537d3f72516869ac9cae9
Andrius <anavasaitis@gmail.com>
2025-08-21 14:05:18 +0300

    change both stack-a and stack-b

--- stack-b/main.tf
+++ stack-b/main.tf
@@ -1,1 +1,14 @@
+resource "random_string" "db_connection_string" {
+  length  = 16
+  special = false
+}
 
+output "DB_CONNECTION_STRING" {
+  value = "random11"
+  sensitive   = true
+}
+
+output "DB_CONNECTION_STRING_2" {
+  value = "random11"
+  sensitive   = true
+}

720c4b9c0676f39a62a9436fc7c4c362c16d5495
Andrius <anavasaitis@gmail.com>
2025-10-27 14:36:07 +0200

    undo dep delete

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ resource "spacelift_stack" "stack-3" {
   branch       = "main"
 }
 
-//resource "spacelift_stack_dependency" "dependency" {
-//  stack_id            = spacelift_stack.stack-2.id
-//  depends_on_stack_id = spacelift_stack.stack-1.id
-//}
+resource "spacelift_stack_dependency" "dependency" {
+  stack_id            = spacelift_stack.stack-2.id
+  depends_on_stack_id = spacelift_stack.stack-1.id
+}
 
-//resource "spacelift_stack_dependency_reference" "reference" {
-//  stack_dependency_id = spacelift_stack_dependency.dependency.id
-//  output_name         = "DB_CONNECTION_STRING"
-//  input_name          = "TF_VAR_APP_DB_URL"
-//}
+resource "spacelift_stack_dependency_reference" "reference" {
+  stack_dependency_id = spacelift_stack_dependency.dependency.id
+  output_name         = "DB_CONNECTION_STRING"
+  input_name          = "TF_VAR_APP_DB_URL"
+}
